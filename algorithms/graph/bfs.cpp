dd783e7fc14006eaa452fbe2c3334924973ed5c8
klxu03 <beaver2025@gmail.com>
2023-02-02 14:01:28 -0500

    Tabbed helper functions in data structures

--- algorithms/graph/bfs.cpp
+++ algorithms/graph/bfs.cpp
@@ -1,21 +1,21 @@
-void bfs(long long starting_node) {
-    deque<long long> dq;
-    dq.push_back(starting_node);
-    visited[starting_node] = true;
+    void bfs(long long starting_node) {
+        deque<long long> dq;
+        dq.push_back(starting_node);
+        visited[starting_node] = true;
 
-    // dist array for each node distance from starting node
-    vector<long long> dist(nodes); 
+        // dist array for each node distance from starting node
+        vector<long long> dist(nodes); 
 
-    while(!dq.empty()) {
-        long long current = dq.front();
-        dq.pop_front();
+        while(!dq.empty()) {
+            long long current = dq.front();
+            dq.pop_front();
 
-        for (long long neighbor : adj[current]) {
-            if (visited[neighbor] == false) {
-                visited[neighbor] = true;
-                dq.push_back(neighbor);
-                dist[neighbor] = dist[current] + 1;
+            for (long long neighbor : adj[current]) {
+                if (visited[neighbor] == false) {
+                    visited[neighbor] = true;
+                    dq.push_back(neighbor);
+                    dist[neighbor] = dist[current] + 1;
+                }
             }
         }
     }
-}
