aa93d10d3da8c7b7a7be1d7ca9b0eec24901c533
klxu03 <beaver2025@gmail.com>
2023-02-01 19:19:09 -0500

    Add DSU into template

---
+++ data-structures/DSU.cpp
@@ -0,0 +1,31 @@
+class DSU {
+    public:
+    vector<int> c; // c for components
+
+    DSU(int n) {
+        c = vector<int>(n, -1);
+    }
+
+    // get representive component (uses path compression)
+	int get(int x) { 
+        return c[x] < 0 ? x : c[x] = get(c[x]); 
+    }
+
+    bool same_set(int a, int b) { 
+        return get(a) == get(b); 
+    }
+
+    int size(int x) { 
+        return -c[get(x)]; 
+    }
+
+    bool unite(int x, int y) {  // union by size
+		x = get(x), y = get(y);
+		if (x == y) return false; // same group
+		if (c[x] > c[y]) swap(x, y); // if c[x] is less negative, or greater than c[y]
+		
+        c[x] += c[y]; 
+        c[y] = x;
+		return true;
+	}
+};
