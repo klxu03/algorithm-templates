646a4f0fe009cf34da6803be9ec6d70201264165
klxu03 <beaver2025@gmail.com>
2023-02-24 22:10:00 -0500

    Commented out possibly unneeded immediately things

--- simple.cpp
+++ simple.cpp
@@ -4,42 +4,42 @@
 
 #include <string>
 
-#include <sstream>
+// #include <sstream>
 
 #include <iterator>
 
-#include <map>
+// #include <map>
 
-#include <set>
+// #include <set>
 
-#include <utility>
+// #include <utility>
 
 #include <algorithm>
 
-#include <unordered_map>
+// #include <unordered_map>
 
-#include <queue>
+// #include <queue>
 
-#include <functional>
+// #include <functional>
 
-#include <array>
+// #include <array>
 
 #include <deque>
 
-#include <climits>
+// #include <climits>
 
-#include <stdio.h>
+// #include <stdio.h>
 
-#include <stdlib.h>
+// #include <stdlib.h>
 
 using namespace std;
 using ll = long long;
 
 #define f0r(a, b) for (long long a = 0; a < b; a++)
-#define f1r(a, b, c) for (long long a = b; a < c; a++)
-#define pb push_back
-#define f first
-#define s second
+// #define f1r(a, b, c) for (long long a = b; a < c; a++)
+// #define pb push_back
+// #define f first
+// #define s second
 
 #define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
